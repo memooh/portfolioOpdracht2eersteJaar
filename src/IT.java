d05fcf4d55f48124d4c4bfab57e21cbe346d592e
memooh <m.ovdb@outlook.com>
2021-06-11 18:20:55 +0200

    IT Functie korter gemaakt + BetterCodeHub + CodeSmells

--- src/IT.java
+++ src/IT.java
@@ -11,13 +11,7 @@ class IT extends Dealer{
         System.out.print ("Moet de ontwikkelaar Java/PHP/Symfony/HTML/C+ weten? (j/n) ");
         Scanner scanner = new Scanner(System.in);
         if(scanner.nextLine().equals("j")){
-            return "Ontwikkelaar heeft ervaring met:" + "\r\n" +
-                    "Java" + "\r\n" +
-                    "PHP" + "\r\n" +
-                    "Symfony" + "\r\n" +
-                    "HTML" + "\r\n" +
-                    "C+" + "\r\n"
-                    ;
+            return "Ontwikkelaar heeft ervaring met:" + "\r\n" + "Java" + "\r\n" + "PHP" + "\r\n" + "Symfony" + "\r\n" + "HTML" + "\r\n" + "C+" + "\r\n";
         }else if(scanner.nextLine().equals("n")){
             return "Ontwikkelaar heeft geen enof geen ervaring met een van de genoemde talen."+ "\r\n";
         }
